78d1859bce1899bcc1810f71e95527b5e1f4bdc7
HansRichard <74646853+HansRichard@users.noreply.github.com>
2023-03-08 15:04:36 +0100

    yeet

--- src/environment.ts
+++ src/environment.ts
@@ -20,8 +20,8 @@ class Object3D {
     collidesWith(other: Object3D): boolean {
         const distance = Math.sqrt(
             (other.x - this.x) ** 2 +
-                (other.y - this.y) ** 2 +
-                (other.z - this.z) ** 2
+            (other.y - this.y) ** 2 +
+            (other.z - this.z) ** 2
         );
 
         return distance < this.radius + other.radius;
@@ -43,36 +43,55 @@ class Environment {
     private dronePositionHistory: Object3D[];
     private drone: Object3D;
 
-    constructor() {
-        this.environment = [];
-        this.drone = new Object3D(0, 0, 0, 20);
-    }
+    public EnvironmentX: intrang = 200;
+    public EnvironmentY: number = 200;
+    type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+    ? Acc[number]
+    : Enumerate<N, [...Acc, Acc['length']]>
+
+type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
+
+type T = IntRange<20, 300>;
+
+
+constructor() {
+    this.environment = [];
+    this.drone = new Object3D(0, 0, 0, 20);
+}
 
     public addObject(x: number, y: number, z: number, r: number) {
-        this.environment.push(new Object3D(x, y, z, r));
-    }
+    this.environment.push(new Object3D(x, y, z, r));
+}
 
     public updateDronePosition(droneState: DroneState) {
-        this.drone.x = droneState.x;
-        this.drone.y = droneState.y;
-        this.drone.z = droneState.z;
+    this.drone.x = droneState.x;
+    this.drone.y = droneState.y;
+    this.drone.z = droneState.z;
 
-        this.dronePositionHistory.push(
-            new Object3D(this.drone.x, this.drone.y, this.drone.z, 2)
-        );
-    }
+    this.dronePositionHistory.push(
+        new Object3D(this.drone.x, this.drone.y, this.drone.z, 2)
+    );
+}
 
     public serialize() {
-        return JSON.stringify({
-            dronePositionHistory: this.dronePositionHistory.map((position) =>
-                position.serialize()
-            ),
-            environment: this.environment.map((object) => object.serialize()),
-            drone: this.drone.serialize(),
-        });
-    }
+    return JSON.stringify({
+        dronePositionHistory: this.dronePositionHistory.map((position) =>
+            position.serialize()
+        ),
+        environment: this.environment.map((object) => object.serialize()),
+        drone: this.drone.serialize(),
+    });
+}
 }
 
 export default {
     environment: new Environment(),
 };
+
+
+class Fly extends Environment {
+
+
+
+
+}
