b64802f63e967f557686d9436a5f7eace47ab8c1
Jaffenheimer <jfog20@student.aau.dk>
2023-03-16 14:17:18 +0100

    Added rotation camera and lights #siuuuuuuu

--- src/www/scripts/3dmap.js
+++ src/www/scripts/3dmap.js
@@ -5,14 +5,15 @@ const mapCanvas3D = $("#map");
 const fov = 75;
 const aspect = 4 / 3;
 const near = 0.1;
-const far = 10;
+const far = 1000;
 const boxWidth = 1;
 const boxHeight = 1;
 const boxDepth = 1;
 const color = 0xffffff;
-const intensity = 1;
+const intensity = 1.5;
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+const cameraSpeed = 1;
 const cubes = [
     make3DCubeInstance(geometry, 0x44aa88, 0),
     make3DCubeInstance(geometry, 0x8844aa, -2),
@@ -22,8 +23,12 @@ const cubes = [
 function render3DCube() {
     let time = 0;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 4;
+    const cameraOffset = new THREE.Vector3(0, 7, 5);
+    camera.position.x = cameraOffset.x;
+    camera.position.y = cameraOffset.y;
+    camera.position.z = cameraOffset.z;
 
+    camera.lookAt(0, 0, 0);
     const renderer = new THREE.WebGLRenderer({
         antialias: true,
         canvas: mapCanvas3D,
@@ -34,7 +39,8 @@ function render3DCube() {
     scene.add(light);
     const amb = new THREE.AmbientLight();
     amb.intensity = 0.5;
-    scene.add(amb);
+    //scene.add(amb);
+    scene.add(new THREE.GridHelper(20, 20));
 
     function animateCubes() {
         time += 0.01; // convert time to seconds
@@ -44,6 +50,12 @@ function render3DCube() {
             cube.rotation.x = rotation;
             cube.rotation.y = rotation;
         });
+        camera.position.x =
+            cameraOffset.length() * Math.sin(time * cameraSpeed);
+        camera.position.z =
+            cameraOffset.length() * Math.cos(time * cameraSpeed);
+        camera.lookAt(0, 0, 0);
+        light.position.set(...camera.position);
         renderer.render(scene, camera);
         requestAnimationFrame(animateCubes);
     }
@@ -57,7 +69,7 @@ function make3DCubeInstance(geometry, color, x) {
     scene.add(cube);
 
     cube.position.x = x;
-
+    cube.position.y = 2;
     return cube;
 }
 
