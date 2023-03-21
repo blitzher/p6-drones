0e9d9d6976547fffd260c0eb569ac8b4694439ca
Jaffenheimer <jfog20@student.aau.dk>
2023-03-21 10:10:40 +0100

    Small changes to 3d map, and added glfloader library

--- src/www/scripts/3dmap.js
+++ src/www/scripts/3dmap.js
@@ -5,7 +5,7 @@ const mapCanvas3D = $("#map");
 const fov = 75;
 const aspect = 4 / 3;
 const near = 0.1;
-const far = 1000;
+const far = 100;
 const boxWidth = 1;
 const boxHeight = 1;
 const boxDepth = 1;
@@ -13,17 +13,13 @@ const color = 0xffffff;
 const intensity = 1.5;
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-const cameraSpeed = 1;
-const cubes = [
-    make3DCubeInstance(geometry, 0x44aa88, 0),
-    make3DCubeInstance(geometry, 0x8844aa, -2),
-    make3DCubeInstance(geometry, 0xaa8844, 2),
-];
+const cameraSpeed = 0.3;
+const cubes = [];
 
 function render3DCube() {
     let time = 0;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    const cameraOffset = new THREE.Vector3(0, 7, 5);
+    const cameraOffset = new THREE.Vector3(0, 20, 15);
     camera.position.x = cameraOffset.x;
     camera.position.y = cameraOffset.y;
     camera.position.z = cameraOffset.z;
@@ -40,7 +36,7 @@ function render3DCube() {
     const amb = new THREE.AmbientLight();
     amb.intensity = 0.5;
     //scene.add(amb);
-    scene.add(new THREE.GridHelper(20, 20));
+    scene.add(new THREE.GridHelper(100, 100));
 
     function animateCubes() {
         time += 0.01; // convert time to seconds
@@ -50,8 +46,10 @@ function render3DCube() {
             cube.rotation.x = rotation;
             cube.rotation.y = rotation;
         });
-        camera.position.x = cameraOffset.length() * Math.sin(time * cameraSpeed);
-        camera.position.z = cameraOffset.length() * Math.cos(time * cameraSpeed);
+        camera.position.x =
+            cameraOffset.length() * Math.sin(time * cameraSpeed);
+        camera.position.z =
+            cameraOffset.length() * Math.cos(time * cameraSpeed);
         camera.lookAt(0, 0, 0);
         light.position.set(...camera.position);
         renderer.render(scene, camera);
@@ -73,7 +71,7 @@ function make3DCubeInstance(size, pos, color) {
 
 function clearCubes() {
     for (let cube of cubes) {
-        cubes.pop;
+        cubes.pop();
     }
 }
 
